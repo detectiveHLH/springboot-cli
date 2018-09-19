d1153d6f72064078f3b5aa725120301bb828876d
hulunhao <hulunhao@T4F-MBP-18117.local>
2018-09-19 14:08:31 +0800

    Implement basic operation

---
+++ packages/commands/message.js
@@ -0,0 +1,10 @@
+const messages = {
+    // 失败提示
+    DIR_ALREADY_EXIST: 'Error:\nThe directory is already exist!',
+    LACK_OF_NAME: 'Error:\nPlease enter the name of this new project!',
+
+    // 成功提示
+    SUCCESS: 'The project has been initialized successfully',
+};
+
+module.exports = messages;
--- packages/commands/module.js
+++ packages/commands/module.js
@@ -2,15 +2,35 @@ const path = require('path');
 const fs = require('fs');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
+const childProcess = require('child_process');
+
+const messages = require('./message');
 
 /**
  * 生成新的项目
+ * Author: detectiveHLH
  */
 const generateProject = (config, name) => {
-    console.log(name);
-    console.log(process.cwd());
-    let targetPath = `${process.cwd()}/${name}`;
-    fs.mkdirSync(targetPath);
+    // 当前的目录下新建用户自定的文件夹
+    if (name) {
+        let targetPath = `${process.cwd()}/${name}`;
+        if (fs.existsSync(targetPath)) {
+            // 目录已经存在
+            console.log(chalk.red(messages.DIR_ALREADY_EXIST));
+        } else {
+            // 执行创建操作
+            fs.mkdirSync(targetPath);
+            const command = `git clone git@github.com:detectiveHLH/springbootdemo.git ./${name} && rm -rf ./${name}/.git`;
+            childProcess.exec(command, (error, stdout, stderr) => {
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
+            });
+            console.log(chalk.greenBright('success '), chalk.whiteBright(messages.SUCCESS));
+        }
+    } else {
+        console.log(chalk.red(messages.LACK_OF_NAME));
+    }
 };
 
 module.exports = generateProject;
